0ea6e33a1eaf90528a8ea5a59a1c2263f329c90d
efb <evanbarden@gmail.com>
2022-05-10 21:17:48 -0400

    solutions for 02-object-properties

--- 02-object-properties/object-properties.js
+++ 02-object-properties/object-properties.js
@@ -1,1 +1,36 @@
 /* eslint-disable no-unused-vars */
+
+function setPropsOnObj(object) {
+  object.x = 7;
+  object['y'] = 8;
+  object.onePlus = (num) => {
+    return ++num;
+  };
+}
+
+function setPropsOnArr(arrayObject) {
+  (arrayObject.hello = () => {
+    return 'Hello!';
+  }),
+    (arrayObject.full = 'stack');
+  arrayObject[0] = 5;
+  arrayObject.twoTimes = (num) => {
+    return num * 2;
+  };
+}
+
+function setPropsOnFunc(functionObject) {
+  functionObject.year = '20??';
+  functionObject.divideByTwo = (num) => {
+    return num / 2;
+  };
+}
+
+function shallowCopy(obj1, obj2) {
+  if (Array.isArray(obj1) && Array.isArray(obj2)) {
+    //return obj1.concat(obj2);
+    return [...obj1, ...obj2];
+  }
+
+  return { ...obj1, ...obj2 };
+}
--- 02-object-properties/object-syntax-practice/object-syntax.js
+++ 02-object-properties/object-syntax-practice/object-syntax.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-unused-vars */
+const loud = 'loud';
+const name = 'Cody';
+
+const dog = {
+  bark() {
+    return 'ruff ruff!';
+  },
+  name,
+  [`${loud}Bark`]() {
+    return this.bark().toUpperCase();
+  },
+};
+
+// const { bark, loudBark } = dog;
+// console.log(bark);
+// console.log(loudBark);
